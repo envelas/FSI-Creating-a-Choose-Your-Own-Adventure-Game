2c9573e91debf31219dfcc2f60051e7f66379a7d
Enrique Velasco <enriquevelascomail@gmail.com>
2022-01-19 21:17:43 -0800

    update 3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.prompt('Do you head left or right?')// Your Code Herecd
-let left  = window.prompt('Do you head left or right?')
-console.log(left)
+let answer  = window.prompt('Do you head left or right?')
+let firstAnswer  = window.prompt('Do you head left or right?')
+console.log(firstAnswer)
 
